73720b1ac04c3f354455630addc196397315d772
zaharkoragin <koryagin03@icloud.com>
2022-03-11 14:17:43 +0300

    Initial commit

---
+++ bot/messages.py
@@ -0,0 +1,15 @@
+
+#Сообщения
+help_message = 'Я умею показывать погоду, напиши узнать погоду чтобы проверить'
+start_message = 'Привет, я очередной бот созданный очередным программистом, в качестве pet проекта\n' + help_message
+try_again = 'Попробуйте снова'
+next_tip = 'Идем дельше?'
+
+
+#словарь сообщений
+MESSAGES = {
+    'start': start_message,
+    'help': help_message,
+    'try_again': try_again,
+    'next_tip': next_tip,
+}
