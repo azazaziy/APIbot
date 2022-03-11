89270d9ce9cf01e383eabed71f5fff5e2812b48b
zaharkoragin <koryagin03@icloud.com>
2022-03-11 14:43:54 +0300

    ssl

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_sslify import SSLify
+
+#СОЗДАНИЕ СЕРВЕРА
 app = Flask(__name__)
+sslify = SSLify(app)
 
 @app.route('/')
 def index(username):
     return "Hello, %s!" % username
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4567)
+    app.run()
